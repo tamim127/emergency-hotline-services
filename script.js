af07a61704003d4741c69b3be1439f2e987ffac8
tamim123 <eishatahmedtamim70@gmail.com>
2025-08-29 19:45:44 +0600

    added Copy button Functionalities

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ for (let btn of heartButtons) {
         heartCount.innerText = current + 1;
     });
 }
+
+// Copy button Functionalities
+
+const copyButtons = document.querySelectorAll(".copy-btn");
+const copyCount = document.getElementById("copy-count");
+
+
+copyButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+
+        const card = button.closest(".card");
+        const number = card.querySelector(".argent-num").textContent;
+
+
+        navigator.clipboard.writeText(number)
+            .then(function () {
+
+                let count = parseInt(copyCount.textContent);
+                count = count + 1 + " ";
+                copyCount.textContent = count;
+
+                alert("Number Copied successfully!");
+            })
+            .catch(function (err) {
+                console.error("Failed to copy:", err);
+            });
+    });
+});
